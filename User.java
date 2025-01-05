d21c454edab488452f61c86c104572cdcfd2e108
natalie-eizips <104609729+natalie-eizips@users.noreply.github.com>
2025-01-05 18:02:49 +0200

    Fix

--- User.java
+++ User.java
@@ -58,13 +58,10 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (this.follows[this.follows.length - 1] != null) {
+        if (this.follows(name)) {
             return false;
         }
         for (int i = 0; i < this.follows.length; i++){
-            if (this.follows[i].equals(name)) {
-                return false;
-            }
             if (this.follows[i] == null){
                 this.follows[i] = name;
                 this.fCount++; 
